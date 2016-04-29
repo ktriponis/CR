0e45546d4144422ae6a7e28e5683f16fabe8ed2e
Kęstutis Triponis <kestutis.triponis@gmail.com>
2016-04-29 05:08:31 +0300

    Move post into an entity

---
+++ src/entity/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+class Post
+{
+    private $author;
+    private $message;
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+}
--- src/index.php
+++ src/index.php
@@ -1,7 +1,36 @@
 <?php
 require_once 'autoload.php';
 
+spl_autoload_register(function ($class) {
+    $path = 'entity/' . $class . '.php';
+    if (file_exists($path))
+        /** @noinspection PhpIncludeInspection */
+        include $path;
+});
+
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
 
-$twig->display('index.twig', array('posts' => array(1, 2, 3)));
+$posts = array();
+
+$post = new Post();
+$post->setAuthor('Vardenis Pavardenis');
+$post->setMessage('Sveikas Pasauli!');
+$posts[] = $post;
+
+$post = new Post();
+$post->setAuthor('Geltonas Inkaras');
+$post->setMessage('Skęstu!');
+$posts[] = $post;
+
+$post = new Post();
+$post->setAuthor('Pukuotoji Gervė');
+$post->setMessage('Negali būti');
+$posts[] = $post;
+
+$post = new Post();
+$post->setAuthor('Geltonas Inkaras');
+$post->setMessage(';(');
+$posts[] = $post;
+
+$twig->display('index.twig', array('posts' => $posts));
