bccf413bbddd61230e400fcde7c419bd9862202b
admin <admin>
2016-04-29 15:11:37 +0300

    .

--- src/ClassLoader.php
+++ src/ClassLoader.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function ($class) {
-    $class = substr($class, strrpos($class, '\\') + 1);
+    $class = substr($class, strrpos($class, '\') + 1);
     $classDirectories = array('repository', 'controller', 'entity');
     foreach ($classDirectories as $dir) {
         $path = __DIR__ . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $class . '.php';
